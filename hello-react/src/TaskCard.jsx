786b88fefde15bd3714d65172818fc56c84cee86
Yas1th005 <yashpanatala@gmail.com>
2024-09-17 10:36:21 +0530

    Three

--- hello-react/src/TaskCard.jsx
+++ hello-react/src/TaskCard.jsx
@@ -2,13 +2,13 @@ const TaskCard = (props) => {
     return (
         <>
             {props.completedAtDate === "None" ? (
-                <div className='p-4 border border-gray-300 rounded-lg mb-4 bg-gray-100'>
+                <div className='p-4 border border-black rounded-lg mb-4 text-left'>
                     <p className="font-bold text-lg text-black">{props.title}</p>
                     <p className="text-sm text-black">Due on: {props.dueDate}</p>
                     <p className="text-sm text-black">Assignee: {props.assigneeName}</p>
                 </div>
             ) : (
-                <div className='p-4 border border-gray-300 rounded-lg mb-4 bg-gray-100'>
+                <div className='p-4 border border-black rounded-lg mb-4 text-left'>
                     <p className="font-bold text-lg text-black">{props.title}</p>
                     <p className="text-sm text-black">Completed on: {props.completedAtDate}</p>
                     <p className="text-sm text-black">Assignee: {props.assigneeName}</p>
