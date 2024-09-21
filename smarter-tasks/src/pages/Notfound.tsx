24264bf169718c9b4a3a17c130e3a10244e7a50e
Yas1th005 <yashpanatala@gmail.com>
2024-09-22 01:36:10 +0530

    Four

--- smarter-tasks/src/pages/Notfound.tsx
+++ smarter-tasks/src/pages/Notfound.tsx
@@ -1,24 +1,3 @@
-<<<<<<< HEAD:smarter-tasks/src/pages/NotFound.tsx
-import { Link } from "react-router-dom";
-
-
-
-const NotFound = () => {
-    
-    
-    return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
-        <Link to={"/"}><button id="backToHomeButton">
-          Back to Home
-        </button></Link>
-        
-      </div>
-    );
-  };
-  
-=======
 import { Link } from "react-router-dom";
 
 
@@ -38,5 +17,4 @@ const NotFound = () => {
     );
   };
   
->>>>>>> 776b855d46fa1d821cdd6727e91074193292f09c:smarter-tasks/src/pages/Notfound.tsx
   export default NotFound;
